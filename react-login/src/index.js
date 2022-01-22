b94e004617e01ca80df0f41ac5cdb80b69e1151c
nattapong <nattapong130247@gmail.com>
2022-01-23 00:53:57 +0700

    web page

---
+++ react-login/src/index.js
@@ -0,0 +1,28 @@
+// Components
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Login'
+import Album from './Album'
+import Register from './Register'
+
+// Contents
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/album" element={<Album />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
